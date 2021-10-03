cfdf12988fc7f2c5d4ad88b486bb5d80bf2c0228
theronsterr17 <tom.ron@madmensoftware.com>
2021-10-03 17:40:07 -0400

    added lineup component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './Tree-Club-House.svg';
 import './App.css';
 import Home from './components/Home';
+import Lineup from './components/Lineup';
 
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <Lineup/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h3>
---
+++ src/components/Lineup/index.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import './style.css';
+
+const Lineup = () => {
+    return (
+        <div className="flyer-container">
+                <div className="lineup-grid-container">
+                    <div className="lineup-time">
+                        <p>1:00 - 2:00</p>
+                    </div>
+                        <p>...</p>
+                    <div className="lineup-name">
+                        <p>Ashley Madison x AEYE</p>
+                    </div>
+                </div>
+                <div className="lineup-grid-container">
+                    <div className="lineup-time">
+                        <p>2:00 - 3:00</p>
+                    </div>
+                        <p>...</p>
+                    <div className="lineup-name">
+                        <p>Chozen x Behn</p>
+                    </div>
+                </div>
+                <div className="lineup-grid-container">
+                    <div className="lineup-time">
+                        <p>3:00 - 4:00</p>
+                    </div>
+                        <p>...</p>
+                    <div className="lineup-name">
+                        <p>FrankyV x Ty Abbot</p>
+                    </div>
+                </div>
+                <div className="lineup-grid-container">
+                    <div className="lineup-time">
+                        <p>4:00 - 5:00</p>
+                    </div>
+                        <p>...</p>
+                    <div className="lineup-name">
+                        <p>Luke Whelan x TIKI</p>
+                    </div>
+                </div>
+                <div className="lineup-grid-container">
+                    <div className="lineup-time">
+                        <p>5:00 - 6:00</p>
+                    </div>
+                        <p>...</p>
+                    <div className="lineup-name">
+                        <p>Franky Keen x Ty Abbot</p>
+                    </div>
+                </div>
+        </div>
+    )
+}
+
+export default Lineup;
